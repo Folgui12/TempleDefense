b8ae0648a2aa2acd882f32677b3d3e4a3dcc4c5a
Folgui12 <folguerasbenjamin@gmail.com>
2024-06-25 17:49:47 -0300

    ThunderHabilityWorking

--- Assets/ShootThunder.cs
+++ Assets/ShootThunder.cs
@@ -8,29 +8,38 @@ public class ShootThunder : MonoBehaviour
     [SerializeField] private GameObject ThunderToSpawn;
     [SerializeField] private Transform ShootPoint;
     [SerializeField] private Material HandChargedMaterial;
-
+    [SerializeField] private GameObject HandLaser;
+    
     public bool canShoot = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     { // PROBAR
-        if(TestInputController.Instance._rightController.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButton) && gripButton &&
-            TestInputController.Instance._rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool ThumbButton) && ThumbButton && canShoot)
+        if(TestInputController.Instance._rightController.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButton))
         {
-            canShoot = false;
-            Shoot();
+            if(gripButton)            
+                HandLaser.SetActive(true);
+            
+            else
+                HandLaser.SetActive(false);
+
+            if (TestInputController.Instance._rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool ThumbButton) && 
+                ThumbButton && canShoot)
+            {
+                canShoot = false;
+                Shoot();
+            }
         }
+            
     }
 
     private void Shoot()
     {
-        Debug.Log("Dispare");
         Instantiate(ThunderToSpawn, ShootPoint.position, ShootPoint.rotation);
     }
 
--- Assets/ThunderMovement.cs
+++ Assets/ThunderMovement.cs
@@ -1,20 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ThunderMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
 
+    [SerializeField] private float lifeTime;
+    [SerializeField] private float explosionRadius;
+    [SerializeField] private float explosionForce;
+    [SerializeField] private int explosionDamage;
+
+    private float lifeCounter = 0;
+
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+
+        lifeCounter += Time.deltaTime;
+
+        if(lifeCounter > lifeTime)
+            Destroy(gameObject);
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Tag"))
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            var surroundedEnemies = Physics.OverlapSphere(transform.position, explosionRadius);
+
+            foreach(var enemy in surroundedEnemies)
+            {
+                var getRB = enemy.GetComponent<Rigidbody>();
+                var getEnemyModel = enemy.GetComponent<BaseEnemyModel>();
+
+                if (getRB == null) continue;
+
+                if(getEnemyModel != null)
+                {
+                    getEnemyModel.TakeDamage(explosionDamage);
+                }
+
+                getRB.AddExplosionForce(explosionForce, transform.position - new Vector3(0, 1.5f, 0), explosionRadius);
+            }
+
             Destroy(gameObject);
+        }
+            
     }
 }
