df65d0239b28836075aca83acec0cd25a9ef3d4c
Maximo Samperio <maximosamperio01@gmail.com>
2024-08-20 13:00:40 -0300

    Added Sound Manager
    Sound manager created as a singleton. Allowing it to be called from any script with instances. To use just:
    
    AudioManager.Instance.Play("SoundName");

---
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+
+        public bool loop;
+
+        // Sliders in Inspector for volume and pitch
+        [Range(0f, 1f)] public float volume = 1f;
+        [Range(-3f, 3f)] public float pitch = 1f;
+
+        [HideInInspector] public AudioSource source;
+    }
+
+    public List<Sound> sounds;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.loop = sound.loop;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound != null)
+        {
+            sound.source.Play();
+        }
+        else
+        {
+            Debug.LogWarning("-----Sound: " + name + " not found!-----");
+        }
+    }
+
+    public void Pause(string name)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound != null)
+        {
+            sound.source.Pause();
+        }
+        else
+        {
+            Debug.LogWarning("-----Sound: " + name + " not found!-----");
+        }
+    }
+
+    public void Stop(string name)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound != null)
+        {
+            sound.source.Stop();
+        }
+        else
+        {
+            Debug.LogWarning("-----Sound: " + name + " not found!-----");
+        }
+    }
+
+    public void SetVolume(string name, float volume)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound != null)
+        {
+            sound.source.volume = volume;
+        }
+        else
+        {
+            Debug.LogWarning("-----Sound: " + name + " not found!-----");
+        }
+    }
+
+    public void SetPitch(string name, float pitch)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound != null)
+        {
+            sound.source.pitch = pitch;
+        }
+        else
+        {
+            Debug.LogWarning("-----Sound: " + name + " not found!-----");
+        }
+    }
+
+    public void SetLoop(string name, bool loop)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound != null)
+        {
+            sound.source.loop = loop;
+        }
+        else
+        {
+            Debug.LogWarning("-----Sound: " + name + " not found!-----");
+        }
+    }
+
+}
