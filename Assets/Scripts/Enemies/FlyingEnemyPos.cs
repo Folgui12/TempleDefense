2f984fef6e434ddd30669adbe7e41b6f23ce00fe
Folgui12 <folguerasbenjamin@gmail.com>
2024-09-16 18:06:27 -0300

    Working in the flying enemy

--- Assets/Scripts/Enemies/FlyingEnemyPos.cs
+++ Assets/Scripts/Enemies/FlyingEnemyPos.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,38 @@ using UnityEngine;
 public class FlyingEnemyPos : MonoBehaviour
 {
     [SerializeField] private Transform FloorPos;
+    [SerializeField] private BaseEnemyModel model;
+
+    [SerializeField] private float TransformAir;
+    [SerializeField] private Animator animator;
+
     void Update()
     {
-        transform.position = new Vector3(FloorPos.position.x, transform.position.y, FloorPos.position.z);
+        if (model.gameObject.activeInHierarchy && !model.OnHand)
+        {
+            if(model.OnGround)
+            {
+                transform.position = new Vector3(FloorPos.position.x, TransformAir, FloorPos.position.z);
+                Debug.Log("Moving");
+            }
+        }
+        else
+        {
+            model.gameObject.SetActive(false);
+        }
+    }
+
+    public void MoveModel()
+    {
+        FloorPos.position = new Vector3(transform.position.x, FloorPos.position.y, transform.position.z);
+        model.gameObject.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.gameObject.CompareTag("Floor"))
+        {
+            animator.SetTrigger("HitGround");
+        }
     }
 }
