727030c4c569d7db45f4d783f9dbe4e51370989c
Folgui12 <folguerasbenjamin@gmail.com>
2024-10-01 18:16:36 -0300

    Tutorial Advance

--- Assets/Scripts/TutorialRelateed/TutorialManager.cs
+++ Assets/Scripts/TutorialRelateed/TutorialManager.cs
@@ -8,11 +8,26 @@ public class TutorialManager : MonoBehaviour
     public static TutorialManager Instance;
 
     public Camera CameraRef;
-    public bool FirstTimeSeing;
-    public bool FirstSteps;
-    public bool FirstRotation;
-    public bool FirstPinch;
-    public bool FirstPoke;
+
+    [SerializeField] private GameObject textView;
+    [SerializeField] private GameObject textMove;
+    [SerializeField] private GameObject textRotation;
+    [SerializeField] private GameObject textPinch;
+    [SerializeField] private GameObject textPoke;
+
+    public float walkTime;
+    private float walkTimer;
+
+    public float rotationRounds;
+    public float delayBetweenRotationTime;
+    private float rotationCounter;
+    private float delayBetweenRotationTimer;
+
+    private bool FirstTimeSeing;
+    private bool FirstSteps;
+    private bool FirstRotation;
+    private bool FirstPinch;
+    private bool FirstPoke;
     public bool FirstFistClose;
 
     private void Awake()
@@ -31,17 +46,55 @@ public class TutorialManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        if(FirstTimeSeing && !FirstSteps)
+        {
+            if(TestInputController.Instance._leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 leftJoystick) && leftJoystick.magnitude != 0)
+            {
+                walkTimer += Time.deltaTime;
+                if(walkTimer > walkTime)
+                {
+                    FirstStepsDone();
+                }
+            }
+        }
+        if(FirstSteps && !FirstRotation)
+        {
+            delayBetweenRotationTimer += Time.deltaTime;
+            if (TestInputController.Instance._rightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 rightJoystick) && rightJoystick.x != 0)
+            {
+                if(delayBetweenRotationTimer > delayBetweenRotationTime)
+                {
+                    rotationCounter++;
+                    delayBetweenRotationTimer = 0;
+                }
+                Debug.Log(rotationCounter);
+                if (rotationCounter >= rotationRounds)
+                {
+                    FirstRotationsDone();
+                }
+            }
+        }
     }
 
-    public void StartTutorial()
+    public void FirstRotationsDone()
     {
+        FirstRotation = true;
+        textRotation.SetActive(false);
+        textPinch.SetActive(true);
+    }
 
+    public void FirstStepsDone()
+    {
+        FirstSteps = true;
+        textMove.SetActive(false);
+        textRotation.SetActive(true);
     }
 
     public void VisionTutoDone()
     {
-        FirstTimeSeing = true; 
+        FirstTimeSeing = true;
+        textView.SetActive(false);
+        textMove.SetActive(true);
     }
 
 }
--- Assets/ViewCube.cs
+++ Assets/ViewCube.cs
@@ -5,30 +5,41 @@ using UnityEngine;
 public class ViewCube : MonoBehaviour
 {
     public LayerMask mask;
+    public float fullTime;
 
     private RaycastHit hit;
-    private Quaternion correctedRayForward;
+
+    private float timeView;
+    private float r;
+
+    private void Start()
+    {
+        r = 10;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        correctedRayForward = Quaternion.AngleAxis(transform.rotation.eulerAngles.y, Vector3.up);
-
-        //if (Physics.Raycast(transform.position, correctedRayForward, out hit, 100f))
-        //{
-        //    if (hit.collider.gameObject.layer == 18)
-        //    {   
-        //        hit.collider.gameObject.SetActive(false);
-        //        this.enabled = false;
-        //    }
-        //}
-        
-        Debug.DrawRay(transform.position, transform.forward * 100.0f, Color.green);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100f))
+        {
+            if (hit.collider.gameObject.layer == 18)
+            {
+                timeView += Time.deltaTime;
+                r -= Time.deltaTime;  
+                hit.collider.GetComponent<MeshRenderer>().material.color = new Color(r/10, 1, 1);
+                if(timeView > fullTime)
+                {
+                    TutorialManager.Instance.VisionTutoDone();
+                    hit.collider.gameObject.SetActive(false);
+                    this.enabled = false;
+                }
+            }
+        }
     }
 
-    /*private void OnDrawGizmos()
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z * 50));
-    }*/
+        Gizmos.DrawLine(transform.position, transform.forward * 100.0f);
+    }
 }
